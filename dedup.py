98224b4ca3c0843cd6e91b7067b51a9ec0d8ddf8
toltman <toltman@gmail.com>
2020-01-06 08:05:13 -0500

    added dedup.py and subdomains.py to examine the ouptut of the program

---
+++ dedup.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv('links.csv')
+
+df.nunique()
---
+++ subdomains.py
@@ -0,0 +1,14 @@
+import re
+from urllib.parse import urlparse
+import pandas as pd
+
+hostnames = []
+
+with open("edgov.log") as logfile:
+    for line in logfile:
+        m = re.search(r'DEBUG\: Crawled \(200\) <GET (.*)>', line)
+        if m:
+            hostnames.append(urlparse(m.group(1)).hostname)
+
+s = pd.Series(hostnames)
+print(s.value_counts())
