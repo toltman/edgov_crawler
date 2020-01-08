11c3a1cf10dfea7334a2b4c30f20197dcc8dd80a
toltman <toltman@gmail.com>
2020-01-08 09:17:28 -0500

    removed errback testing spider class

--- edgov/spiders/edgov_spider.py
+++ edgov/spiders/edgov_spider.py
@@ -87,7 +87,7 @@ class EdgovSpider(scrapy.Spider):
                     'data_file': link.url
                 }
         for next_page in edgov_extractor.extract_links(response):
-            yield scrapy.Request(next_page, self.parse, errback=self.parse_error)
+            yield scrapy.Request(next_page.url, self.parse, errback=self.parse_error)
 
     def parse_error(self, failure):
 
@@ -109,49 +109,3 @@ class EdgovSpider(scrapy.Spider):
             with open('errors.csv', 'a+', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(['TimeoutError', request.url])
-
-
-class ErrbackSpider(scrapy.Spider):
-    # this class is for testing
-    name = "errback_example"
-    start_urls = [
-        "http://www.httpbin.org/",              # HTTP 200 expected
-        "http://www.httpbin.org/status/404",    # Not found error
-        "http://www.httpbin.org/status/500",    # server issue
-        "http://www.httpbin.org:12345/",        # non-responding host, timeout expected
-        "http://www.httphttpbinbin.org/",       # DNS error expected
-    ]
-
-    def start_requests(self):
-        for u in self.start_urls:
-            yield scrapy.Request(u, callback=self.parse_httpbin,
-                                 errback=self.errback_httpbin,
-                                 dont_filter=True)
-
-    def parse_httpbin(self, response):
-        self.logger.info(
-            'Got successful response from {}'.format(response.url))
-        # do something useful here...
-
-    def errback_httpbin(self, failure):
-        # log all failures
-        self.logger.error(repr(failure))
-        self.logger.error(failure.request.meta)
-
-        # in case you want to do something special for some errors,
-        # you may need the failure's type:
-
-        if failure.check(HttpError):
-            response = failure.value.response
-            with open('http_error.csv', 'a+', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow([response.url, response.status])
-
-        elif failure.check(DNSLookupError):
-            # this is the original request
-            request = failure.request
-            self.logger.error('DNSLookupError on %s', request.url)
-
-        elif failure.check(TimeoutError, TCPTimedOutError):
-            request = failure.request
-            self.logger.error('TimeoutError on %s', request.url)
